7fc06530ab59402c0460b6745e0862bf72b520ed
Tae-Ho-Yoo <98486145+Tae-Ho-Yoo@users.noreply.github.com>
2022-03-10 12:00:36 -0500

    Add files via upload

---
+++ Derivatives,Integrals,etc.py
@@ -0,0 +1,17 @@
+def integrals(equation):
+    print('integral')
+
+def derivative(equation):
+    print("derivative")
+
+def series(equation):
+    print("series")
+
+def sequence(equation):
+    print("sequence")
+
+integrals("re")
+derivative("ew")
+series("we")
+sequence("sdf")
+print("main")
